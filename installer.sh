73f39e474c868d588351b386a0deca3abd1be0ad
Karina <karinaanjina@gmail.com>
2024-07-19 16:08:42 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,11 +3,10 @@ sudo apt-get update -y
 sudo apt-get install python -y
 sudo apt-get install build-essential -y
 sudo apt-get install git -y 
-curl -sL https://deb.nodesource.com/setup_8.x -o ~/nodesource_setup.sh
-sudo bash ~/nodesource_setup.sh
-sudo apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo ufw allow 8080
 git clone https://github.com/c9/core.git ~/c9sdk
 sudo bash ~/c9sdk/scripts/install-sdk.sh
-sudo apt-get install npm -y
-npm install forever -g
+mkdir ~/my-projects
+node server.js -p 8080 -a username:password --listen 0.0.0.0 -w /home/c9user/my-projects
