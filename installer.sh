699e8be8d6224c99564ae8ca2b1eee94f2e0187a
Karina <karinaanjina@gmail.com>
2024-07-24 21:16:03 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,8 +5,11 @@ sudo apt-get install build-essential -y
 sudo apt-get install git -y 
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo ufw allow 22/tcp
 sudo ufw allow 8080
-git clone https://github.com/c9/core.git ~/c9sdk
-sudo bash ~/c9sdk/scripts/install-sdk.sh
-mkdir ~/my-projects
-node server.js -p 8080 -a username:password --listen 0.0.0.0 -w /home/c9user/my-projects
+sudo ufw enable
+git clone https://github.com/c9/core.git c9sdk
+cd c9sdk
+scripts/install-sdk.sh
+mkdir ~/aku
+node server.js -p 8080 -a memek:basah --listen 0.0.0.0 -w /root/aku 
